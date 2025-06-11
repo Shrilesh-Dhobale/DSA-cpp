a7bad4caea3af1c6d61c86d9d9a0e0c1b925f1e6
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-06-11 15:12:29 +0530

    isSorted function created

--- Array/secondlargest/check-array-sorted/brute.cpp
+++ Array/secondlargest/check-array-sorted/brute.cpp
@@ -1,1 +1,17 @@
 #include<bits/stdc++.h>
+using namespace std;
+
+bool isSorted(int arr[],n)
+{
+    for(int i=0; i<n ;i++)
+    {
+        for(int j=1; j<n ;j++)
+        {
+            if(arr[j]<arr[i])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
