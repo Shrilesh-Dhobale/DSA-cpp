f49ef1e152bceb01adac09896e2f605111e9bc4f
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-06-11 15:33:25 +0530

    isSorted fun created

--- Array/secondlargest/check-array-sorted/optimal.cpp
+++ Array/secondlargest/check-array-sorted/optimal.cpp
@@ -1,2 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+bool isSorted(int arr[],int n)
+{
+    int i=0;
+    for(int j=1;j<n;j++)
+    {
+        if(arr[i] != arr[j])
+        {
+            return true;
+            i++;
+        }
+    }
+    return false;
+}
