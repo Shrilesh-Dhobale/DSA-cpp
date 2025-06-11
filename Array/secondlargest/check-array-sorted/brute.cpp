0ef3d6d2a6153041e7775bb473f784fca480f40c
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-06-11 15:23:19 +0530

    j=i+1 added

--- Array/secondlargest/check-array-sorted/brute.cpp
+++ Array/secondlargest/check-array-sorted/brute.cpp
@@ -5,7 +5,7 @@ bool isSorted(int arr[],int n)
 {
     for(int i=0; i<n ;i++)
     {
-        for(int j=1; j<n ;j++)
+        for(int j=i+1; j<n ;j++)
         {
             if(arr[j]<arr[i])
             {
