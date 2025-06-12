06bf86f9c01ade51c9895715e70df711b3f095a7
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-06-12 18:39:35 +0530

    removeDup fun created

--- Array/remove-duplicates/optimal.cpp
+++ Array/remove-duplicates/optimal.cpp
@@ -1,2 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int removeDup(int arr[],int n)
+{
+    int i=0;
+    int n=nums.size();
+    for(int j=1;j<n;j++)
+    {
+        if(nums[i]!=nums[j])
+        {
+            i++;
+            nums[i]=nums[j];
+        }
+    }
+    return i+1;//len of unique elements +1 because index of starts from 0
+}
