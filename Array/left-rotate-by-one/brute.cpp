a4ec2f0679b3320fc37f3d67456b81ec2029cf40
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-06-13 20:32:13 +0530

    solve fun created

--- Array/left-rotate-by-one/brute.cpp
+++ Array/left-rotate-by-one/brute.cpp
@@ -1,2 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void solve(int arr[],int n)
+{
+    int temp[n];
+    for(int i=1;i<n;i++)//
+    {
+        temp[n-1]=arr[i];
+    }
+    temp[n-1=arr[0]];//first element of original array
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i];
+    }
+    cout<<endl;
+}
