88a120fe8c9416b99eab65af5b8bb3e0148b3480
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-06-14 19:48:30 +0530

    Prg run successfully

--- Array/left-rotate-by-one/brute.cpp
+++ Array/left-rotate-by-one/brute.cpp
@@ -6,12 +6,12 @@ void solve(int arr[],int n)
     int temp[n];
     for(int i=1;i<n;i++)//siz
     {
-        temp[n-1]=arr[i];
+        temp[i-1]=arr[i];
     }
-    temp[n-1=arr[0]];//first element of original array
+    temp[n-1]=arr[0];//first element of original array
     for(int i=0;i<n;i++)
     {
-        cout<<arr[i];
+        cout<<temp[i]<<" ";
     }
     cout<<endl;
 }
