934b5884ea9361af69c13a667de5c9c3f600a28e
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-07-13 20:54:58 +0530

    loop and cond added

--- Array/smallest-largest/explicit_fun.cpp
+++ Array/smallest-largest/explicit_fun.cpp
@@ -4,5 +4,12 @@ using namespace std;
 int main(){
     int nums[]={5,15,22,1,-15,-24};
     int size=6;
-    
+
+    int smallest=INT32_MAX;
+    int largest=INT32_MIN;
+
+   
+    cout<<"Smallest="<<smallest<<endl;
+    cout<<"Largest="<<largest<<endl;
+    return 0;
 }
