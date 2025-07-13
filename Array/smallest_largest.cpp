18d71b409e54d79aaa195dc46a9d06851d500fdc
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-07-13 20:36:15 +0530

    successfully run

--- Array/smallest_largest.cpp
+++ Array/smallest_largest.cpp
@@ -5,18 +5,22 @@ int main(){
     int nums[]={5,15,22,1,-15,-24};
     int size=6;
 
-    int samllest=INT8_MAX;
+    int smallest=INT8_MAX;
     int largest=INT8_MIN;
 
     for(int i=0;i<size;i++)
     {
-        if(int nums[i] < smallest)
+        if( nums[i] < smallest)
         {
             smallest=nums[i];
         }
-        if(int nums[i] > largest)
+        if( nums[i] > largest)
         {
             largest=nums[i];
         }
     }
+
+    cout<<"Smallest"<<smallest<<endl;
+    cout<<"Largest"<<largest;
+    return 0;
 }
