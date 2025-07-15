a7338c3d0b04ce5bb41cc7c6ed22a33de9071238
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-07-15 09:03:28 +0530

    Function created

--- Array/swap-max-min/swap.cpp
+++ Array/swap-max-min/swap.cpp
@@ -1,2 +1,22 @@
 #include<iostream>
 using namespace std;
+
+int swapMinMax(int arr[],int n)
+{
+    int minIndex=0;
+    int maxIndex=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]>arr[maxIndex])
+        {
+            maxIndex=i;
+        }
+    }
+    for(int j=0;j<n;j++)
+    {
+        if(arr[j]<arr[minIndex])
+        {
+            minIndex=i;
+        }
+    }
+}
